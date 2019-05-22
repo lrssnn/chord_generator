baeb703c116953225498fbe4229f85966b4fc62f
Niklas Larsson <niklaslarsson95@gmail.com>
2019-05-22 15:56:41 +1000

    THis is a false start I think

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
 fn main() {
 
-    println!("Hello, world!");
-
-    println!("Notes:");
-
-    for note in get_note_names(false) {
-        println!("-> {}", note);
+    for root in 2..3 {
+        println!("{} major", spell(root, false));
+        for note in major_scale(root) {
+            print!("{} ", spell(note, false));
+        }
+        println!("{}", if validate_scale(major_scale(root), false) {"good"} else {"bad"});
     }
-
-    println!("Flats:");
-
-    for note in get_note_names(true) {
-        println!("-> {}", note);
-    }
-
 }
 
 fn get_note_names(flats: bool) -> Vec<String> {
     let mut res = vec![];
     for i in 1..13 {
-        res.push(get_note_name(i, flats));
+        res.push(spell(i, flats));
     }
     res
 }
 
-fn get_note_name(note: i32, flat: bool) -> String {
+fn spell(note: i32, flat: bool) -> String {
+    if note > 12 {
+        return spell(note - 12, flat);
+    }
+
     match note {
         1 => "A",
         2 => if flat {"Bb"} else {"A#"}, 
@@ -38,7 +35,32 @@ fn get_note_name(note: i32, flat: bool) -> String {
         10 => if flat {"Gb"} else {"F#"},
         11 => "G",
         12 => if flat {"Ab"} else {"G#"},
-        _  => "NO!",
+        _  => "Invalid",
     }.to_string()
+    
 }
 
+fn major_scale(r: i32) -> Vec<i32> {
+    // T T S T T T S
+    vec![r, r+2, r+4, r+5, r+7, r+9, r+11, r+12]
+}
+
+fn validate_scale(scale: Vec<i32>, flat: bool) -> bool {
+    // We care about whether the scale as written contains consecutive letters e.g. G and G#
+    // This means the scale requires a double sharps/flats and we don't want it
+    
+    for index in 0..7 {
+        let note = scale[index];
+        let next = scale[index + 1];
+
+        print!("({} - {}) ", note, next);
+
+        match note {
+            1 => {if next == 2 {return false;}},
+            _ => (),
+        };
+
+    }
+
+    return true;
+}
